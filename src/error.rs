ad319a167bd34502cf05137f59fd5b4fce738128
Chase Douglas <chasedouglas@gmail.com>
2025-02-18 15:49:04 -0800

    Abstract file access through Apache OpenDAL

--- src/error.rs
+++ src/error.rs
@@ -47,6 +47,7 @@ use lettre::address::AddressError as AddrErr;
 use lettre::error::Error as LettreErr;
 use lettre::transport::smtp::Error as SmtpErr;
 use openssl::error::ErrorStack as SSLErr;
+use opendal::Error as OpenDALErr;
 use regex::Error as RegexErr;
 use reqwest::Error as ReqErr;
 use rocket::error::Error as RocketErr;
@@ -95,6 +96,8 @@ make_error! {
 
     DieselCon(DieselConErr): _has_source, _api_error,
     Webauthn(WebauthnErr):   _has_source, _api_error,
+
+    OpenDAL(OpenDALErr): _has_source, _api_error,
 }
 
 impl std::fmt::Debug for Error {
